857c8e5e194277cd025b7e1d1278ded7577aa927
oskar <case112@users.noreply.github.com>
2026-04-29 20:09:29 +0300

    Wrap data to a table

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,32 @@
-export default async function Home() {
+    export default async function Home() {
+    const data = await fetch(
+        "https://mwr.mikkis.shop/api/lists/all-approvals?dataSource=approvals",
+    );
+    const approvals: any[] = await data.json();
 
-  const data = await fetch('https://mwr.mikkis.shop/api/lists/all-approvals?dataSource=approvals');
-  const approvals: any[] = await data.json();
+    console.log(approvals);
 
-  console.log(approvals);
+    const listApprovals = approvals.map((a) => (
+        <tr  key={a["@unid"]}>
+            <td>{a["@index"]}</td>
+            <td>{new Date(a["$1"]).toLocaleString("et-EE")}</td>
+            <td>{a.Subject}</td>
+            <td>{a["@unid"]}</td>
+        </tr>
+    ));
 
-  const listApprovals = approvals.map(a =>
-    <li key={a["@unid"]}>{a.Subject}</li>
-  );
-
-  return (
-    <main>
-      <div>
-        
-        <br></br>
-        {listApprovals &&
-          <ul>{listApprovals}</ul>
-        }
-      </div>
-    </main>
-  );
-}
+    return (
+        <main>
+        <div>
+            <br></br>
+            {listApprovals && 
+            <table border={1} cellPadding={2} style={{ borderStyle: "dotted", borderRadius: 5}} >
+                <tbody>
+                {listApprovals}
+                </tbody>
+            </table>
+            }
+        </div>
+        </main>
+    );
+    }
